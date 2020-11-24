a81b4d68946c76006edf35c1fcfc355541e974e7
Tawfeeq Amro <tawfeekamr@Tawfeeqs-MacBook-Pro.local>
2020-11-25 00:07:14 +0200

    add form features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import users from './api/users';
+
+
+class App extends React.Component {
+
+    state = {
+        users: [],
+        currentUser: '',
+        userPassword: '',
+        err: ''
+    }
+    usersFetched = async () => {
+        const data = await users;
+        // console.log('Before setSate',data);
+        this.setState({users: data});
+        // console.log('State Value',this.state.users);
+    }
+
+    setCurrentUser = (event) => {
+        // Set the user to state if it exist.
+        this.setState({currentUser: event.target.value})
+    }
+    setPassword = (event) => {
+        // Set the password to state if it exist.
+        this.setState({userPassword: event.target.value})
+    }
+
+
+    checkUsers = () => {
+        // check if the user founded
+        const userFounded = this.state.users.find(user => user.userName === this.state.currentUser);
+        const passwordMatch = this.state.users.find(user => user.Password === this.state.userPassword);
+
+        if(userFounded && passwordMatch) {
+            console.log('the user logged in', this.state.currentUser);
+            console.log('the user pass', this.state.userPassword);
+        } else {
+            console.log('Filed to login');
+        }
+
+    }
+
+    onSubmitData = (event,currentUser) => {
+        // Submit the from data
+        event.preventDefault();
+        this.usersFetched();
+        this.checkUsers(currentUser);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onSubmitData} action="">
+                    <input type="text" value={this.state.currentUser} onChange={this.setCurrentUser}/>
+                    <input type="password" value={this.state.userPassword} onChange={this.setPassword}/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default App;
